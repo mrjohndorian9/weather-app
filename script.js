ee6ccd6d33fa7420f7cfc37cd1bcf6a11078a0f8
mrjohndorian9 <mrjohndorian9@gmail.com>
2023-04-04 11:34:40 +0600

    add ugly unfinished version

--- script.js
+++
@@ -1,8 +0,0 @@
-async function getWeather(place) {
-  const url = `https://api.weatherapi.com/v1/current.json?key=915ce40b87ca4f7381a183907232903&q=${place}&aqi=no`;
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-}
-
-getWeather('Omsk');
